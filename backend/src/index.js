60e2730b2be78c7e3aa287d1a7dbfa28aa376e94
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2025-09-05 15:18:52 +0530

    4

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.use(express.static(frontendBuildPath));
 
-    app.get("*", (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(frontendBuildPath, "index.html"));
     });
 }
